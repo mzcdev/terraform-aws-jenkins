a5d17b295d5b4e1b46db643b3de6237605225c5a
nalbam <me@nalbam.com>
2019-09-16 15:32:16 +0900

    private_ip

--- env/dev-mz-demo/output.tf
+++ env/dev-mz-demo/output.tf
@@ -4,6 +4,10 @@ output "name" {
   value = module.jenkins.name
 }
 
+output "private_ip" {
+  value = module.gitlab.private_ip
+}
+
 output "dns_name" {
   value = module.jenkins.dns_name
 }
--- modules/jenkins/99-output.tf
+++ modules/jenkins/99-output.tf
@@ -8,6 +8,10 @@ output "key_name" {
   value = aws_instance.this.key_name
 }
 
+output "private_ip" {
+  value = aws_instance.this.private_ip
+}
+
 output "dns_name" {
   value = local.dns_name
 }
